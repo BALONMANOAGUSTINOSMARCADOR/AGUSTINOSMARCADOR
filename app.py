8e8aaea404beef57599ffad9ea4c3cd91899bfad
BALONMANOAGUSTINOSMARCADOR <miguelangel@autocares-martinez.com>
2026-01-30 20:56:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -374,10 +374,11 @@ with st.container():
 
     # margen mínimo para pegarlo arriba pero sin bloquear botones
     fig.update_layout(
-        height=520,
-        autosize=True,
-        margin=dict(l=0, r=0, t=0, b=5)
-    )
+    height=380,           # más estrecho verticalmente
+    width=800,            # un poco más ancho
+    autosize=False,       # desactivamos autosize para respetar width/height
+    margin=dict(l=0, r=0, t=-10, b=0)  # sin márgenes
+)
 
     st.plotly_chart(fig, use_container_width=True)
     
