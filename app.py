3fd5320526f63c2100201f8be1690b757debde86
BALONMANOAGUSTINOSMARCADOR <miguelangel@autocares-martinez.com>
2026-01-30 21:01:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -375,7 +375,7 @@ with st.container():
     # margen mínimo para pegarlo arriba pero sin bloquear botones
     fig.update_layout(
     height=380,           # más estrecho verticalmente
-    width=800,            # un poco más ancho
+    width=1500,           # un poco más ancho
     autosize=False,       # desactivamos autosize para respetar width/height
     margin=dict(l=0, r=0, t=0, b=0)  # sin márgenes
 )
