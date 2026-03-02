fd00de5bb92aa6fcda6524e485b9d77741cfbecd
BALONMANOAGUSTINOSMARCADOR <miguelangel@autocares-martinez.com>
2026-03-02 23:24:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -731,8 +731,11 @@ import json
 filename = f"data/partidos/partido_{datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')}.json"
 contenido_json = json.dumps(partido_guardado, indent=4)
 
+# usar tu token de secrets
 g = Github(st.secrets["GITHUB_TOKEN"])
-BALONMANOAGUSTINOSMARCADOR/AGUSTINOSMARCADOR
+
+# obtener el repo correcto
+repo = g.get_repo("BALONMANOAGUSTINOSMARCADOR/AGUSTINOSMARCADOR")
 
 repo.create_file(
     filename,
