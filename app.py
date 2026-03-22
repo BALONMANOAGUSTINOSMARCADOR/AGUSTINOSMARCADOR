4adda6e1446d20a7414b0c4cff49a14a10e5ec00
BALONMANOAGUSTINOSMARCADOR <miguelangel@autocares-martinez.com>
2026-03-22 08:30:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -677,7 +677,8 @@ def draw_fintas(team):
                 arrowwidth=1,
                 arrowcolor="gray",
                 text=f"{pct_dcha}%",
-                font=dict(size=10),
+                font=dict(size=11, color="black"),
+                text=f"<b>{texto}</b>",
             )
 
             # 🔽 FINTA IZ (flecha inferior)
@@ -691,7 +692,8 @@ def draw_fintas(team):
                 arrowwidth=1,
                 arrowcolor="gray",
                 text=f"{pct_iz}%",
-                font=dict(size=10),
+                font=dict(size=11, color="black"),
+                text=f"<b>{texto}</b>",
             )
 
         # ================================
